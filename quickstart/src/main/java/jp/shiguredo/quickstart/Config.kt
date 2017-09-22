98ec1701e6b5a994e0ded8d546659d45ef9c9715
Shunichi Shinohara <shino@shiguredo.jp>
2017-09-22 18:38:35 +0900

    signaling endpoint を build.gradle で設定する
    ついでに gradle.properties で上書き可能にする。
    gradle.properties は .gitignore で除外設定されている。

--- quickstart/src/main/java/jp/shiguredo/quickstart/Config.kt
+++ quickstart/src/main/java/jp/shiguredo/quickstart/Config.kt
@@ -1,9 +1,11 @@
 package jp.shiguredo.quickstart
 
+import android.os.Build
+
 class Conf {
 
     companion object {
-        const val SIGNALING_ENDPOINT = "wss://sora.example.com/signaling"
+        const val SIGNALING_ENDPOINT = BuildConfig.SIGNALING_ENDPOINT
         const val CHANNEL_ID = "sora"
     }
 }
