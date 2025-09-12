c008b52ab25ed3f84ca2b7e46b12449b743f4d52
t-miya <tatumiya008005@gmail.com>
2025-09-12 12:26:03 +0900

    sora-android-sdk を composite build で取り込む

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "sora-android-sdk-quickstart"
 include(":quickstart")
+
+// ローカルの sora-sdk-android のパスを指定
+val soraSdkDirPath = ""
+// ローカル SDK を composite build で取り込む
+includeBuild(soraSdkDirPath) {
+    dependencySubstitution {
+        substitute(module("com.github.shiguredo:sora-android-sdk")).using(project(":sora-android-sdk"))
+    }
+}
